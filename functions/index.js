0f5635e7ab68bd5a216783fe4684cd1cb6ef71d2
Nicholas Guest <nicholasguest@MacBook-Pro.local>
2017-08-20 14:02:00 -0700

    initial commit

--- functions/index.js
+++ functions/index.js
@@ -13,8 +13,49 @@
 // limitations under the License.
 
 var functions = require('firebase-functions');
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+var fetch = require('node-fetch');
+//var admin = require("firebase-admin");
+// var serviceAccount = require("path/to/serviceAccountKey.json");
+//
+// var config = {
+//     apiKey: "AIzaSyDdTmZPfamocJYLs3ce7tnHA_12niJBfCk",
+//     authDomain: "terracetemp2.firebaseapp.com",
+//     databaseURL: "https://terracetemp2.firebaseio.com",
+//     projectId: "terracetemp2",
+//     storageBucket: "terracetemp2.appspot.com",
+//     messagingSenderId: "607134304579"
+//   };
+//   firebase.initializeApp(config);
 
 exports.hourly_job =
   functions.pubsub.topic('hourly-tick').onPublish((event) => {
     console.log("This job is ran every hour!")
+  });
+
+exports.minute_job =
+  functions.pubsub.topic('five-minute-tick').onPublish((event) => {
+    console.log("This job goes each 5 MINUTES! -")
+    var URL='https://api.spark.io/v1/devices/53ff71066667574808382467/data?access_token=d1d253a2a98001d4df65eee7d919e0d9149a2ae7';
+
+    fetch(URL)
+      .then(function(data){
+        return data.json()
+      })
+      .then(function(datajson){
+        console.log('datajson',datajson)
+        if (!datajson.error) {
+          var timestamp = new Date().getTime()
+          admin.database().ref('dataset').child(timestamp).set(datajson);
+        }
+      })
+      .catch(function(error){
+        console.log('ERR',error)
+      })
+  //  var timestamp = (Math.random()*999999).toFixed(0)
+
   });
